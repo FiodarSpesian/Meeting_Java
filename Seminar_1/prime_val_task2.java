d4e1e271471d4ef330df56cf612a82687277bf31
Fiodar <fiodarspesian@mail.ru>
2023-03-08 09:40:29 +0300

    add decision of task 2

--- Seminar_1/prime_val_task2.java
+++ Seminar_1/prime_val_task2.java
@@ -1,11 +1,7 @@
 // Вывести все простые числа от 1 до 1000
 package Seminar_1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class task_2 {
+public class prime_val_task2 {
     public static void main(String[] args) {
         Integer[] arr = new Integer[1000];
         StringBuilder sb = new StringBuilder();
