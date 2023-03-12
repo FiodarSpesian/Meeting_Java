d63d0b587448d7090a5376d9a1e7259f559dfb14
Fiodar <fiodarspesian@mail.ru>
2023-03-12 11:08:32 +0300

    add Examp Sem 4 task_1

---
+++ Examples/Seminar_4/task_1.java
@@ -0,0 +1,33 @@
+package Examples.Seminar_4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class task_1 {
+    public static void main(String[] args) {
+        Scanner iscan = new Scanner(System.in);
+        LinkedList<String> ll=new LinkedList<>();
+        while(true){
+            String n =iscan.nextLine();
+            if(n.equals("stop")){
+                break;
+            }
+            else{
+                if(n.contains("print~")){
+                    ArrayList<String> str = new ArrayList(Arrays.asList(n.split("~")));
+                    int a=Integer.parseInt(str.get(1));
+                    System.out.println(ll.get(a));
+                    ll.remove(a);            
+                    }
+                else{
+                    ll.add(n);
+                }
+                System.out.println(ll);
+            }
+    
+        }
+    }
+}
+
