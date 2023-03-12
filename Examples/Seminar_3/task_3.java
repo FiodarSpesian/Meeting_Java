b907d1e7a7c71a88e678f00f5aa4de24738622cd
Fiodar <fiodarspesian@mail.ru>
2023-03-12 13:28:04 +0300

    Examp 3 task_3

--- Examples/Seminar_3/task_3.java
+++ Examples/Seminar_3/task_3.java
@@ -11,10 +11,10 @@ public class task_3 {
     Пройти по списку, найти и удалить целые числа.
      */
     public static void main(String[] args) {
-        ArrayList MyList = new ArrayList<>(Arrays.asList("9", "text", 5, "anouther text", 25, 23.25));
+        ArrayList MyList = new ArrayList<>(Arrays.asList(1, "9", "text", 5, 5, "anouther text", 25, 23.25));
         for (int i = 0; i < MyList.size(); i++) {
             if(MyList.get(i) instanceof Integer){
-                MyList.remove(MyList.get(i));
+                MyList.remove(MyList.get(i--));
             }
         }
         System.out.println(MyList);
