87d80f420f14fbe390e4a5bd19c8350f1eaa0ae8
Fiodar <fiodarspesian@mail.ru>
2023-03-12 15:58:46 +0300

    add decision of task_2 Seminar 3

---
+++ Seminar_3/task_1.java
@@ -0,0 +1,7 @@
+package Seminar_3;
+// 1 Задание. (Дополнительное) Реализовать алгоритм сортировки слиянием
+public class task_1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Seminar_3/task_2.java
@@ -0,0 +1,20 @@
+package Seminar_3;
+
+import java.util.ArrayList;
+
+// 2 Задание. Пусть дан произвольный список целых чисел, удалить из него чётные числа
+public class task_2 {
+    public static void main(String[] args) {
+        ArrayList <Integer> lst = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            lst.add((int)(Math.random()*10));
+        }
+        System.out.println(lst);
+        for (int i = lst.size() - 1; i >= 0; i--) {
+            if(lst.get(i) % 2 == 0){
+                lst.remove(i);
+            }
+        }
+        System.out.println(lst);
+    }
+}
