c1da1cc6bf925fe8cd6c4f5ccd1f3df50e9edf0c
Fiodar <fiodarspesian@mail.ru>
2023-03-12 16:58:42 +0300

    add task_3 seminar 3

--- Seminar_3/task_3.java
+++ Seminar_3/task_3.java
@@ -3,19 +3,31 @@ package Seminar_3;
 // Найти минимальное, максимальное и среднее ариф. из этого списка
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class task_3 {
     public static void main(String[] args) {
+        System.out.println("Enter size of array: ");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        sc.close();
         Integer min = 0;
         Integer max = 0;
         Integer medArif = 0;
-        ArrayList<Integer> lst =  new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        ArrayList<Integer> lst =  new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
             lst.add((int)(Math.random()*10));
         }
         System.out.println(lst);
+        min = lst.get(0);
         for (int i = 0; i < lst.size(); i++) {
-            if (lst(i) > max) max = lst[
+            if (lst.get(i) > max) max = lst.get(i);
+            if (lst.get(i) < min) min = lst.get(i);
+            medArif = medArif + lst.get(i);
+            System.out.println(medArif);
         }
+        System.out.println("Max value of array: " + max);
+        System.out.println("Min value of array: " + min);
+        System.out.println("Average value of array: " + (medArif/lst.size()));
     }
 }
