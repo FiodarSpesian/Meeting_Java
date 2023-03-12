fbd8aa5cabcc402d09824389141dbb4959362eba
Fiodar <fiodarspesian@mail.ru>
2023-03-12 16:23:03 +0300

    correct task_2 seminar 3

--- Seminar_3/task_2.java
+++ Seminar_3/task_2.java
@@ -1,17 +1,18 @@
 package Seminar_3;
 
 import java.util.ArrayList;
+import java.util.List;
 
 // 2 Задание. Пусть дан произвольный список целых чисел, удалить из него чётные числа
 public class task_2 {
     public static void main(String[] args) {
-        ArrayList <Integer> lst = new ArrayList<>();
+        List lst = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             lst.add((int)(Math.random()*10));
         }
         System.out.println(lst);
         for (int i = lst.size() - 1; i >= 0; i--) {
-            if(lst.get(i) % 2 == 0){
+            if((int)lst.get(i) % 2 == 0){
                 lst.remove(i);
             }
         }
--- Seminar_3/task_3.java
+++ Seminar_3/task_3.java
@@ -1,6 +1,21 @@
 package Seminar_3;
 // 3 Задание. Задан целочисленный список ArrayList.
 // Найти минимальное, максимальное и среднее ариф. из этого списка
+
+import java.util.ArrayList;
+
 public class task_3 {
-    
+    public static void main(String[] args) {
+        Integer min;
+        Integer max;
+        Integer medArшf;
+        ArrayList<Integer> lst =  new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            lst.add((int)(Math.random()*10));
+        }
+        System.out.println(lst);
+        for (int i = 0; i < lst.size(); i++) {
+            
+        }
+    }
 }
