1896a0030a27429dcd2422494d1b603d3bbc5535
Fiodar <fiodarspesian@mail.ru>
2023-03-12 13:51:00 +0300

    add decision of task_4 seminar_1

--- Seminar_1/task_4.java
+++ Seminar_1/task_4.java
@@ -15,15 +15,17 @@ import java.util.Scanner;
 public class task_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter expression with '?' by the number: ");
-        String exp = sc.nextLine();
-        String[] ast = exp.split("");
-        System.out.println(Arrays.toString(ast));
-        sc.close();
-        for (int i = 0; i < ast.length; i++) {
-            if(ast[i] == "?"){
-                System.out.println(i);    
+        System.out.print("Enter equation like '2? + ?5 = 69': ");
+        String equation = sc.nextLine (); //2? + ?5 = 69
+        String message = "Решения нет!"; 
+        for (int i = 0; i < 10; i++) {
+            String result = equation. replace ("?", Integer. toString (i));
+            // 20 + 05 = 69
+            String[] arr = result. split( " ");
+            if (Integer.parseInt(arr [0]) + Integer.parseInt (arr [2]) == Integer.parseInt (arr [4])){
+                message = result;
             }
         }
+        System.out.println(message);
     }
 }
