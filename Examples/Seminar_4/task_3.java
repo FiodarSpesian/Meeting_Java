06630fdee353459484b46f4bd10335c6de8d1876
Fiodar <fiodarspesian@mail.ru>
2023-03-14 07:46:10 +0300

    add decision ES4T3

--- Examples/Seminar_4/task_3.java
+++ Examples/Seminar_4/task_3.java
@@ -1,5 +1,35 @@
 package Examples.Seminar_4;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class task_3 {
-    
+    /*
+    Реализовать консольное приложение, которое:
+    1. Принимает от пользователя и "запоминает" строки.
+    2. Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.(Stack)
+    3. stop - Заканчивает работу программы
+     */
+    public static void main(String[] args) {
+        Scanner iscan = new Scanner(System.in);
+        Stack<String> stack = new Stack<>();
+        while(true){
+            String n = iscan.nextLine();
+            if(n.equals("stop")){
+                System.out.println("End of the program.");
+                iscan.close();
+                break;
+            }
+            else {
+                if(n.equals("print")){
+                    while (!stack.isEmpty()) {
+                        System.out.println(stack.pop());
+                    }
+                }
+                else{
+                    stack.push(n);
+                }
+            }
+        }
+    }
 }
