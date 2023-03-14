5bb8f45a33194995b6c60ae76657680bc76f4b9f
Fiodar <fiodarspesian@mail.ru>
2023-03-14 07:08:58 +0300

    correct ES4T2

--- Examples/Seminar_4/task_2.java
+++ Examples/Seminar_4/task_2.java
@@ -6,6 +6,13 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class task_2 {
+    /*
+    Реализовать консольное приложение, которое:
+    Принимает от пользователя строку вида: text
+    Нужно сохранить text в связный список (LinkedList).
+    Если введено print~num, выводит строку из позиции num в связном списке и удаляет её из списка.
+    Если введено stop то программа завершается.
+     */
     public static void main(String[] args) {
         Scanner iscan = new Scanner(System.in);
         LinkedList<String> ll=new LinkedList<>();
@@ -27,7 +34,6 @@ public class task_2 {
                 }
                 System.out.println(ll);
             }
-    
         }
     }
 }
