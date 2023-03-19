5b27ff2e2c799d264806c3ec3746ae9a6c7cface
Fiodar <fiodarspesian@mail.ru>
2023-03-19 10:59:26 +0300

    ES6T2

--- Examples/Seminar_6/task_2.java
+++ Examples/Seminar_6/task_2.java
@@ -1,5 +1,9 @@
 package Examples.Seminar_6;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.TreeSet;
+
 public class task_2 {
     /*
     1. Напишите метод, который заполнит массив из 100 элементов случайными цифрами от 0 до 100.
@@ -11,6 +15,32 @@ public class task_2 {
     процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
      */
     public static void main(String[] args) {
-        
+        int len = 100;
+        int[] original = new int[len];
+        original = fillArray(original, 0, 100);
+        for (int i = 0; i < original.length; i++) {
+            System.out.print(original[i] + " ");
+
+        }
+        System.out.println();
+        System.out.println(countUnique(original));
+    }
+
+    public static int[] fillArray(int[] array, int min, int max) {
+        for (int i = 0; i < array.length; i++) {
+            // Random ran = new Random();
+            // array_to_fill.add(ran.nextInt(0, 100)); // генерация рандома
+            array[i] = (int) (Math.random() * (max + 1 - min)) + min;
+        }
+        return array;
+    }
+    public static double countUnique(int[] array) {
+        TreeSet<Integer> unique = new TreeSet<>();
+        for (int i = 0; i < array.length; i++) {
+            unique.add(array[i]);
+        }
+        System.out.println(unique);
+        double uniqueAmount = unique.size() * 100 / array.length;
+        return uniqueAmount;
     }
 }
