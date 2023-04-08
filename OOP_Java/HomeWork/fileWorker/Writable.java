94b654144e5c2b8e3f01eadc0e314772ae423fb3
Fiodar <fiodarspesian@mail.ru>
2023-04-08 09:30:01 +0300

    add folders view, presenter, model

--- OOP_Java/HomeWork/fileWorker/Writable.java
+++
@@ -1,7 +0,0 @@
-package OOP_Java.HomeWork.fileWorker;
-
-import java.io.IOException;
-
-public interface Writable {
-    public void save() throws IOException;
-}
--- OOP_Java/HomeWork/human/Gender.java
+++
@@ -1,7 +0,0 @@
-package OOP_Java.HomeWork.human;
-
-import java.io.Serializable;
-
-public enum Gender implements Serializable{
-    Male, Female
-}
